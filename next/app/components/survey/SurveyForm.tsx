0ac6d1dec41cc0c6e10e8fd9ea56adacbc2c7438
EthanRohman93 <ethanmrohman@gmail.com>
2024-05-15 10:44:13 -0500

    deplOyMeNt8

--- next/app/components/survey/SurveyForm.tsx
+++ next/app/components/survey/SurveyForm.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState, FormEvent } from 'react';
-import Question from './Question';
-import SurveySubmit from './SurveySubmit';
+import SurveyButton from './SurveyButton';
 import { useRouter } from 'next/navigation';
 
 interface QuestionData {
@@ -14,75 +13,87 @@ interface FormDataMap {
 }
 
 const SurveyForm = () => {
-const router = useRouter();
-    const questions: QuestionData[] = [
-        { questionText: "What are your thoughts on the color scheme of the website?", name: "color_scheme" },
-        { questionText: "How do you find the layout and organization of the website?", name: "layout_organization" },
-        { questionText: "How would you rate the execution and performance of the website?", name: "execution_performance" },
-        { questionText: "What is your experience with the interactivity of the website?", name: "website_interactivity" },
-        { questionText: "Did you experience any confusion while navigating our website?", name: "navigation_confusion" },
-    ];
-    
-    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-    const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+  const questions: QuestionData[] = [
+    { questionText: "What are your thoughts on the color scheme of the website?", name: "color_scheme" },
+    { questionText: "How do you find the layout and organization of the website?", name: "layout_organization" },
+    { questionText: "How would you rate the execution and performance of the website?", name: "execution_performance" },
+    { questionText: "What is your experience with the interactivity of the website?", name: "website_interactivity" },
+    { questionText: "Did you experience any confusion while navigating our website?", name: "navigation_confusion" },
+  ];
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setIsSubmitting(true);
-        const formData = new FormData(event.currentTarget);
-        const data: FormDataMap = {};
-        let allAnswered = true;
+  const [formData, setFormData] = useState<FormDataMap>({});
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-        questions.forEach(question => {
-            const value = formData.get(question.name);
-            if (!value) {
-                allAnswered = false;
-                setError(`Please answer the question: "${question.questionText}"`);
-            }
-            data[question.name] = value ? parseInt(value as string) : 0;
-        });
+  const handleRadioChange = (name: string, value: string) => {
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: parseInt(value)
+    }));
+  };
 
-        if (!allAnswered) {
-            setIsSubmitting(false);
-            return;
-        }
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsSubmitting(true);
 
-        fetch('http://18.220.173.252/submit-form/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        }).then(data => {
-            console.log('Success:', data);
-            router.push('/thankyou');
-            setIsSubmitting(false);
-        }).catch((error: any) => {
-            console.error('Error:', error);
-            setError(error.message);
-            setIsSubmitting(false);
-        });
-    };
+    // Check if all questions have been answered
+    if (Object.keys(formData).length !== questions.length) {
+      setError("Please answer all questions.");
+      setIsSubmitting(false);
+      return;
+    }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            {questions.map((question, index) => (
-                <Question
-                    key={index}
-                    questionText={question.questionText}
-                    name={question.name}
+    fetch('http://18.220.173.252/submit-form/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    }).then(data => {
+      console.log('Success:', data);
+      router.push('/thankyou');
+      setIsSubmitting(false);
+    }).catch((error: any) => {
+      console.error('Error:', error);
+      setError(error.message);
+      setIsSubmitting(false);
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {questions.map((question, index) => (
+        <div key={index} className='flex flex-col px-2 py-4'>
+          <label className='text-lg'>{question.questionText}</label>
+          <div className='flex flex-row justify-center'>
+            {[1, 2, 3, 4].map(value => (
+              <div key={value} className='flex flex-col px-10 py-2 items-center'>
+                <input
+                  type="radio"
+                  id={`${question.name}-${value}`}
+                  name={question.name}
+                  value={String(value)}
+                  className='h-5 w-5'
+                  onChange={(e) => handleRadioChange(question.name, e.target.value)}
                 />
+                <label htmlFor={`${question.name}-${value}`}>{value === 1 ? 'Poor' : value === 2 ? 'Fair' : value === 3 ? 'Good' : 'Excellent'}</label>
+              </div>
             ))}
-            <SurveySubmit />
-            {isSubmitting && <p>Submitting...</p>}
-            {error && <p>Error: {error}</p>}
-        </form>
-    );
+          </div>
+        </div>
+      ))}
+      <SurveyButton />
+      {isSubmitting && <p>Submitting...</p>}
+      {error && <p>Error: {error}</p>}
+    </form>
+  );
 };
 
 export default SurveyForm;
+
